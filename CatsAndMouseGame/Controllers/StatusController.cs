bb12f2b35db0cc0cfe27becd9891d3cff85ff3a7
mgimeno <mgimeno@gmail.com>
2025-02-27 21:24:29 +0100

    Fix minor error

--- CatsAndMouseGame/Controllers/StatusController.cs
+++ CatsAndMouseGame/Controllers/StatusController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 
 namespace CatsAndMouseGame.Controllers
