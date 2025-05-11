6d7d2269d2e81edf92f3c68994a5c28dd7aa7565
mgimeno <mgimeno@gmail.com>
2025-05-11 12:17:37 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 var builder = WebApplication.CreateBuilder(args);
 var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins");
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
@@ -41,32 +39,22 @@ builder.Services.AddSignalR(hubOptions =>
         options.PayloadSerializerOptions.WriteIndented = false;
     });
 
-// if (!builder.Environment.IsDevelopment())
-// {
-//     builder.Services.AddHttpsRedirection(options =>
-//     {
-//         options.HttpsPort = 53000;
-//         options.RedirectStatusCode = Status307TemporaryRedirect;
-//     });
-// }
-
 if (!builder.Environment.IsDevelopment()){
     //Set SSL Certificate
     builder.Services.Configure<KestrelServerOptions>(options =>
     {
         var cert = X509Certificate2.CreateFromPemFile ("/home/mgimeno/certificates/marcosgimeno_com_chain.crt", "/home/mgimeno/certificates/marcosgimeno.com.key");
-        //var cert = new X509Certificate2("/", "");
-
         //options.Listen(IPAddress.Any, 80); // http
-        // options.Listen(IPAddress.Any, 443, listenOptions => // https
-        // {
-        //     listenOptions.UseHttps(cert);
-        // });
         options.Listen(IPAddress.Any, 53000, listenOptions => // https
         {
             listenOptions.UseHttps(cert);
         });
     });
+    //     builder.Services.AddHttpsRedirection(options =>
+//     {
+//         options.HttpsPort = 53000;
+//         options.RedirectStatusCode = Status307TemporaryRedirect;
+//     });
 }
 
 builder.WebHost.UseUrls(urls: builder.Environment.IsDevelopment() ? "http://127.0.0.1:53000" : "https://127.0.0.1:53000");
@@ -86,7 +74,7 @@ app.UseForwardedHeaders();
 
 if (!app.Environment.IsDevelopment())
 {
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 }
 
 app.UseRouting();
@@ -97,12 +85,4 @@ app.MapHub<GameHub>("/gameHub");
 
 app.MapControllerRoute("default","{controller=Status}/{action=Status}");
 
-if (app.Environment.IsDevelopment())
-{
-app.Run("http://127.0.0.1:53000");
-}
-else{
-    app.Run("https://127.0.0.1:53000");
-}
-
-
+app.Run(app.Environment.IsDevelopment() ? "http://127.0.0.1:53000" : "https://127.0.0.1:53000");
