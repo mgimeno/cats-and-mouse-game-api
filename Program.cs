b28055361c794cf38b5ae5b8d5529107f526c954
mgimeno <mgimeno@gmail.com>
2026-01-28 13:11:27 +0100

    HTTP instead of HTTPS

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
+//using System.Security.Cryptography.X509Certificates;
 using CatsAndMouseGame.Hubs;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -39,22 +39,20 @@ builder.Services.AddSignalR(hubOptions =>
         options.PayloadSerializerOptions.WriteIndented = false;
     });
 
-if (!builder.Environment.IsDevelopment()){
-    //Set SSL Certificate
-    builder.Services.Configure<KestrelServerOptions>(options =>
-    {
-        var cert = X509Certificate2.CreateFromPemFile ("/home/mgimeno/certificates/marcosgimeno_com_chain.crt", "/home/mgimeno/certificates/marcosgimeno.com.key");
-        //options.Listen(IPAddress.Any, 80); // http
-        options.Listen(IPAddress.Any, 53000, listenOptions => // https
-        {
-            listenOptions.UseHttps(cert);
-        });
-    });
-    //     builder.Services.AddHttpsRedirection(options =>
+ if (!builder.Environment.IsDevelopment()){
+//     // Set SSL Certificate for HTTPS
+//     builder.Services.Configure<KestrelServerOptions>(options =>
 //     {
-//         options.HttpsPort = 53000;
-//         options.RedirectStatusCode = Status307TemporaryRedirect;
+//         var cert = X509Certificate2.CreateFromPemFile ("/home/mgimeno/certificates/marcosgimeno_com_chain.crt", "/home/mgimeno/certificates/marcosgimeno.com.key");
+//         
+//         options.Listen(IPAddress.Any, 53000, listenOptions => // https
+//         {
+//             listenOptions.UseHttps(cert);
+//         });
 //     });
+
+        // HTTP
+        options.Listen(IPAddress.Any, 53000); // http
 }
 
 builder.WebHost.UseUrls(urls: builder.Environment.IsDevelopment() ? "http://127.0.0.1:53000" : "https://127.0.0.1:53000");
